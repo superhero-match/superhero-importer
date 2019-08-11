503529d79839e6685521b2a94381ca162aff57db
MWSOFT <mwsoftofficial@gmail.com>
2019-08-11 13:13:04 +0200

    Add internal package

---
+++ internal/config/db.go
@@ -0,0 +1,10 @@
+package config
+
+// DB holds the configuration values for the database.
+type DB struct {
+	Host     string `env:"DB_HOST" default:"127.0.0.1"`
+	Port     int    `env:"DB_PORT" default:"3306"`
+	User     string `env:"DB_USER" default:"root"`
+	Password string `env:"DB_PASSWORD" default:""`
+	Name     string `env:"DB_NAME" default:"superhero_match"`
+}
